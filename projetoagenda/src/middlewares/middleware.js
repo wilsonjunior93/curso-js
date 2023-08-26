66d7b43d66c0bea73b6fce8c4acf548efd6ee007
wilsonjunior <wilsonjunior-pdm@hotmail.com>
2023-08-26 15:43:54 -0300

    Atualizando....

--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -1,19 +1,21 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
-    next();
-  };
-  
-  exports.outroMiddleware = (req, res, next) => {
-    next();
-  };
+  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  next();
+};
 
-  exports.checkCsrfError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code) {
-      return res.send('404');
-    }
-  };
+exports.outroMiddleware = (req, res, next) => {
+  next();
+};
 
-  exports.csrfMiddleware = (req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next();
-  };
+exports.checkCsrfError = (err, req, res, next) => {
+  if (err) {
+    return res.send('404');
+  }
+
+  next();
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
