ae5558fdf4b2aab6493300baa8eaf6e55549a16b
wilsonjunior <wilsonjunior-pdm@hotmail.com>
2023-08-30 12:41:56 -0300

    ProjetoAgenda

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -27,3 +27,32 @@ exports.register = async function(req, res) {
     return res.render('404');
   }
 };
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
+    }
+
+    req.flash('success', 'Você entrou no sistema.');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+};
--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
   next();
 };
 
@@ -9,7 +10,7 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if (err) {
+  if(err) {
     return res.render('404');
   }
 
@@ -18,5 +19,15 @@ exports.checkCsrfError = (err, req, res, next) => {
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
   next();
 };
